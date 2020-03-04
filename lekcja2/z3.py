18f9ad5b88111fa02e6a1117a90713e919699864
piotr <pghost286@gmail.com>
2020-03-04 11:00:41 +0100

    if wart bezwzgledna

---
+++ lekcja2/z3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#dostępne operatory logiczne
+#<= >= ! ==
+
+# używać Bbrake pointów ("czerownych kropek")
+# do podejrzenia operacji wykonanych już na zmiennej
---
+++ lekcja2/z4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+l1=int(input())
+
+if l1 <0:
+    print(-l1)
+else:
+    print(l1)
