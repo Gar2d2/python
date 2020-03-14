bc837f49f7dfef854458671509044cfba04f7e77
Gar2d2 <pghost286@gmail.com>
2020-03-14 12:07:51 +0100

    dswe

--- auto_git.py
+++ auto_git.py
@@ -1,6 +1,6 @@
 import os
 commit= input("wprowadź treść commita: ")
 os.system("git add . ")
-os.system("git commit -m '" + commit + "'")
+os.system("git commit -m " + commit )
 os.system("git push origin master")
 input()
