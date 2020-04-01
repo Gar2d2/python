05e1903ba467c16507119aa5de25d27f4e0eff81
Gar2d2 <pghost286@gmail.com>
2020-04-01 10:33:49 +0200

    rand%4

--- obiekty/ex1.py
+++ obiekty/ex1.py
@@ -2,7 +2,7 @@ import random as r
 plik = open("mod4.txt", "w+")
 i = 0
 rand = r.randrange(0,1000)
-while i < 4: 
+while i < 10:    #generuje 10 liczb z przedziału 0 - 1000 podzielnych przez 4 
     rand = r.randrange(0,1000)
     if (rand % 4) == 0:
         plik.write(str(rand) + " ")
