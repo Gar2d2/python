4b6fe643f307e698907eecb84f13b3f88a107a3d
Gar2d2 <pghost286@gmail.com>
2020-04-12 21:15:17 +0200

    wyrazenie generujace zwracajace miesiace

---
+++ lekcja5/cwi12.py
@@ -0,0 +1,8 @@
+miesiace = (miesiac for miesiac in ["styczen", "luty", "marzec", "kwiecien", "maj"])
+
+
+print(next(miesiace))
+print(next(miesiace))
+print(next(miesiace))
+print(next(miesiace))
+print(next(miesiace))
